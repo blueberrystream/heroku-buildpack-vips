6f43114f01703334d5e4ff0e383a60fa744b4fda
KID the Euforia <kid0725+github@gmail.com>
2022-01-10 17:08:23 +0900

    heroku 20 and libvips 8.12.1 with libpango1.0-dev
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- build.sh
+++ build.sh
@@ -3,18 +3,17 @@
 # set -x
 set -e
 
-STACK_VERSIONS=(16 18 20)
+stack_version=20
+VIPS_VERSION=8.12.1
 
-for stack_version in "${STACK_VERSIONS[@]}"; do
-  image_name=libvips-heroku-$stack_version:$VIPS_VERSION
+image_name=libvips-heroku-$stack_version:$VIPS_VERSION
 
-  docker build \
-    --build-arg VIPS_VERSION=${VIPS_VERSION} \
-    --build-arg STACK_VERSION=${stack_version}\
-    -t $image_name \
-    container
+docker build \
+  --build-arg VIPS_VERSION=${VIPS_VERSION} \
+  --build-arg STACK_VERSION=${stack_version}\
+  -t $image_name \
+  container
 
-  mkdir -p build
+mkdir -p build
 
-  docker run --rm -t -v $PWD/build:/build $image_name sh -c 'cp -f /usr/local/vips/build/*.tar.gz /build'
-done
+docker run --rm -t -v $PWD/build:/build $image_name sh -c 'cp -f /usr/local/vips/build/*.tar.gz /build'
